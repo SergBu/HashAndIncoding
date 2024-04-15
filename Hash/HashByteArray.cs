341d2b2df50972519d3ea53d4ab5f9a2313ca1a9
sergei.budnikov <sergei.budnikov@smartseeds.ru>
2024-04-15 08:31:33 +0300

    Add project files.

---
+++ Hash/HashByteArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash;
+
+public class HashByteArray
+{
+    public static string GetPhoneHash(string phoneManager)
+    {
+        phoneManager = phoneManager
+            .Replace("(", "")
+            .Replace(")", "")
+            .Replace("-", "");
+
+        byte[] bytes = SHA256.HashData(Encoding.UTF8.GetBytes(phoneManager));
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            builder.Append(bytes[i].ToString());
+        }
+        return builder.ToString().Substring(0, 40);
+    }
+
+
+    private string HashData(Entity entity)
+    {
+        var hashString = String.Concat(entity.Id, entity.Type, entity.IsPositive);
+        StringBuilder Sb = new StringBuilder();
+
+        using (SHA256 hash = SHA256.Create()) //хэш функция
+        {
+            Encoding enc = Encoding.UTF8;
+            Byte[] result = hash.ComputeHash(enc.GetBytes(hashString)); //массив байтов
+
+            foreach (Byte b in result)
+                Sb.Append(b.ToString("x2"));
+        }
+        return Sb.ToString();
+    }
+
+    public string CalculateMD5Hash(string input)
+    {
+
+        // Primeiro passo, calcular o MD5 hash a partir da string
+        MD5 md5 = System.Security.Cryptography.MD5.Create();
+        byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+        byte[] hash = md5.ComputeHash(inputBytes);
+
+        // Segundo passo, converter o array de bytes em uma string haxadecimal
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hash.Length; i++)
+        {
+            sb.Append(hash[i].ToString("X2"));
+        }
+        return sb.ToString();
+    }
+}
---
+++ Hash/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hash;
+
+var phoneManager = HashByteArray.GetPhoneHash("8(926)568-95-25");
+
+Console.WriteLine(phoneManager);
+
+//Utf8-16 b5eb67e8d36813029715876b5ffff44daf3047d0 Hexadecimal
+//Utf-8 1812351032322111041921512113510795255244 number
